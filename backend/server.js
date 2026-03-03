66533a65cd7e628be60a74f06f16cd49a56514a8
GrewalBalraj <balraj.grewal.careers@gmail.com>
2026-03-03 01:39:08 -0500

    Database connection and server start

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { default: mongoose } = require('mongoose');
 const path = require('path');
 
 const PORT = 8080;
@@ -9,3 +10,18 @@ const DATABASE_PORT = 27017;
 
 app.use(cors());
 
+// Database connect
+const dbURL = `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/events`;
+mongoose.connect(dbURL);
+
+// Checks if database connection was successful or not and returns message
+const db = mongoose.connection;
+db.on('error', function(e) {
+    console.log('error connecting' + e);
+});
+db.on('open', function() {
+    console.log('database connected!');
+});
+
+// Starts server
+app.listen(PORT, () => { console.log("Server started on port: " + PORT) });
